cb1c3b0eae509ab00e0fe62ddaa59514b41b9851
rhysd <lin90162@yahoo.co.jp>
2012-05-06 07:34:59 +0900

    fix some silly mistakes

--- gfj/array/utility.hpp
+++ gfj/array/utility.hpp
@@ -10,7 +10,7 @@
 #include <type_traits>
 #include "../index_tuple.hpp"
 
-namespace grj{
+namespace gfj{
 
     // make_array: element type is common type of args
     template <class T, class Array>
@@ -49,7 +49,7 @@ namespace grj{
 
     namespace detail{
         template <class T, std::size_t N, std::size_t... Indices>
-        constexpr std::array<T, N> to_array_impl( T const(& native_array)[N], index_tuple<Indices...>)
+        constexpr std::array<T, N> to_array_impl( T const(& native_array)[N], gfj::index_tuple<Indices...>)
         {
             return {{ native_array[Indices]... }};
         }
@@ -58,7 +58,7 @@ namespace grj{
     template <class T, std::size_t N>
     constexpr std::array<T, N> to_array( T const(& native_array)[N])
     {
-        return detail::to_array_impl( native_array, typename index_range<0, N>::type() );
+        return detail::to_array_impl( native_array, typename gfj::index_range<0, N>::type() );
     }
 
     namespace detail{
@@ -75,8 +75,8 @@ namespace grj{
                   std::size_t N1, std::size_t... Indices1,
                   std::size_t N2, std::size_t... Indices2
                 >
-        constexpr std::array<T, N1+N2> joint_array_impl( std::array<T, N1> const& a1, index_tuple<Indices1...>,
-                                              std::array<T, N2> const& a2, index_tuple<Indices2...> )
+        constexpr std::array<T, N1+N2> joint_array_impl( std::array<T, N1> const& a1, gfj::index_tuple<Indices1...>,
+                                              std::array<T, N2> const& a2, gfj::index_tuple<Indices2...> )
         {
             return {{ at(a1, Indices1)..., at(a2, Indices2)... }};
         }
@@ -87,8 +87,8 @@ namespace grj{
     constexpr std::array<T, N1+N2> joint_array( std::array<T, N1> const& a1,
                                                 std::array<T, N2> const& a2 )
     {
-        return detail::joint_array_impl( a1, typename index_range<0, N1>::type(),
-                                         a2, typename index_range<0, N2>::type() );
+        return detail::joint_array_impl( a1, typename gfj::index_range<0, N1>::type(),
+                                         a2, typename gfj::index_range<0, N2>::type() );
     }
 
     /* 引数解決ができずコンパイルエラー．
@@ -96,7 +96,7 @@ namespace grj{
         template< class T, std::size_t N, std::size_t Sep,
                   std::size_t... Indices1, std::size_t... Indices2 >
         constexpr std::pair< std::array<T, Sep>, std::array<T, N-Sep> > 
-                split_array_impl( std::array<T, N> const& a, index_tuple<Indices1...>, index_tuple<Indices2...> )
+                split_array_impl( std::array<T, N> const& a, gfj::index_tuple<Indices1...>, gfj::index_tuple<Indices2...> )
         {
             return std::make_pair( std::array<T, Sep>{{ at(a, Indices1)... }},
                                    std::array<T, N-Sep>{{ at(a, Indices2)... }} );
@@ -106,8 +106,8 @@ namespace grj{
     template< std::size_t Sep, class T, std::size_t N >
     constexpr std::pair< std::array<T, Sep>, std::array<T, N-Sep> > split_array( std::array<T, N> const& a )
     {
-        return detail::split_array_impl( a, typename index_range<0, Sep>::type(),
-                                            typename index_range<Sep, N>::type() );
+        return detail::split_array_impl( a, typename gfj::index_range<0, Sep>::type(),
+                                            typename gfj::index_range<Sep, N>::type() );
     }
     */
 
