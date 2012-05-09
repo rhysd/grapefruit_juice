f6f363ebff0b9105d5ba460069367f2ee9389edf
rhysd <lin90162@yahoo.co.jp>
2012-05-10 04:13:47 +0900

    Type modifications

--- gfj/type_traits/type_traits_aliases.hpp
+++ gfj/type_traits/type_traits_aliases.hpp
@@ -5,6 +5,11 @@
 
 namespace gfj{
 
+    /*
+     * 20.9.7 Transformations between types
+     */
+
+    // 20.9.7.1 Const-volatile modifications
     template <class T>
     using remove_cv = typename std::remove_cv<T>::type;
 
@@ -23,26 +28,37 @@ namespace gfj{
     template <class T>
     using add_volatile = typename std::add_volatile<T>::type;
 
+    // 20.9.7.2 Reference modifications
     template <class T>
     using remove_reference = typename std::remove_reference<T>::type;
 
     template <class T>
-    using add_pointer = typename std::add_pointer<T>::type;
+    using add_lvalue_reference = typename std::add_lvalue_reference<T>::type;
 
     template <class T>
-    using make_signed = typename std::make_signed<T>::type;
+    using add_rvalue_reference = typename std::add_rvalue_reference<T>::type;
 
-    template <class T>
-    using make_unsigned = typename std::make_unsigned<T>::type;
-
+    // 20.9.7.4 Array modifications
     template <class T>
     using remove_extent = typename std::remove_extent<T>::type;
 
     template <class T>
     using remove_all_extents = typename std::remove_all_extents<T>::type;
 
+    // 20.9.7.5 Pointer modifications
+    template <class T>
+    using remove_pointer = typename std::remove_pointer<T>::type;
+
+    template <class T>
+    using add_pointer = typename std::add_pointer<T>::type;
+
+    // 20.9.7.6 Other transformations
     template <std::size_t Len, std::size_t Align>
     using aligned_storage = typename std::aligned_storage<Len, Align>::type;
+
+    /* aligned_union is not implemented in std... */
+    // template <std::size_t Len, class... Types>
+    // using aligned_union = typename std::aligned_union<Len Types...>::type;
 
     template<class T>
     using decay = typename std::decay<T>::type;
@@ -61,9 +77,6 @@ namespace gfj{
 
     template <class F, class... Args >
     using result_of = typename std::result_of<F(Args...)>::type;
-
-    template <class T, T v>
-    using integral_constant = typename std::integral_constant<T, v>::type;
 }
 
 #endif // LINDAPP_TYPE_TRAITS_ALIASES_HPP_INCLUDED__
